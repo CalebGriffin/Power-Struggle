9f667517f1403f8588912ad5c795b6bbbae11e1b
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2021-12-05 23:46:04 +0000

    Implemented Pause and Resume functionality

--- SAO/Assets/Scripts/UIController.cs
+++ SAO/Assets/Scripts/UIController.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     [SerializeField] private Image timerBackground;
+
     private float timePassed;
+
+    [SerializeField] private GameObject pauseCanvas;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +23,22 @@ public class UIController : MonoBehaviour
         timePassed += Time.deltaTime;
         // 60 could easily be replaced with a variable for max time allowed in the level in seconds
         timerBackground.fillAmount = (60-timePassed) / 60; 
+    }
+
+    public void PauseButton()
+    {
+        if (pauseCanvas.activeSelf == false)
+        {
+            Time.timeScale = 0;
+
+            pauseCanvas.SetActive(true);
+        }
+    }
+
+    public void ResumeButton()
+    {
+        Time.timeScale = 1;
+
+        pauseCanvas.SetActive(false);
     }
 }
