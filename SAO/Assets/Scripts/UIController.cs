2ec804412336b331bec853187d9dfac4d4ad3363
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2022-02-03 00:00:43 +0000

    Added Timer Countdown UI Animations

--- SAO/Assets/Scripts/UIController.cs
+++ SAO/Assets/Scripts/UIController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class UIController : MonoBehaviour
 {
@@ -10,10 +11,16 @@ public class UIController : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI resultText; // The text that tells the player whether they are winning, losing or drawing
 
+    [SerializeField] private GameObject numbersTextObj; // The GameObject that has the UI for the timer countdown
+    [SerializeField] private TextMeshProUGUI numbersText; // The text that tells when they are running out of time
+
     // These are the different colours for the text
     private Color whiteColour = Color.white;
     private Color greenColour = new Color(0.1058824f, 1f, 0.03529412f, 1f);
     private Color redColour = new Color(1f, 0.1960784f, 0.09019608f, 1f);
+
+    private bool countdownAnimationStarted = false;
+    private int countdownTime = 10;
 
     [SerializeField] private Image timerBackground; // The background of the timer
 
@@ -26,6 +33,9 @@ public class UIController : MonoBehaviour
     {
         // Get all of the devices currently in the scene
         devices = GameObject.FindGameObjectsWithTag("Device"); 
+
+        numbersText = numbersTextObj.GetComponent<TextMeshProUGUI>();
+        numbersTextObj.SetActive(false);
     }
 
     // Update is called once per frame
@@ -36,6 +46,37 @@ public class UIController : MonoBehaviour
 
         // NOTE: 60 could easily be replaced with a variable for max time allowed in the level in seconds
         timerBackground.fillAmount = (60-timePassed) / 60; 
+
+        if (60 - timePassed <= 10 && !countdownAnimationStarted)
+        {
+            countdownAnimationStarted = true;
+            numbersTextObj.SetActive(true);
+            StartCoroutine(CountdownAnim());
+        }
+
+        if (timePassed >= 60)
+        {
+            GameOver();
+        }
+    }
+
+    private IEnumerator CountdownAnim()
+    {
+        numbersText.text = countdownTime.ToString();
+        numbersTextObj.GetComponent<Animator>().Play("ScaleIn");
+        yield return new WaitForSeconds(0.5f);
+        numbersTextObj.GetComponent<Animator>().Play("ScaleOut");
+        yield return new WaitForSeconds(0.5f);
+        countdownTime--;
+        if (countdownTime > 0)
+        {
+            StartCoroutine(CountdownAnim());
+        }
+    }
+
+    private void GameOver()
+    {
+
     }
 
     // FixedUpdate is called 50 times per second regardless of framerate
